2991f6e800127151afd8296ecfdf552525b1c0ce
khalid <rabaaouikhalid@gmail.com>
2025-11-13 18:07:32 +0100

    add handle files

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,7 @@ const setRefreshTokenCookie = (res, refreshToken) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
