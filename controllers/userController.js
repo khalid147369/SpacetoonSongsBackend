343bd2c1f77ef953b8e2e02d21d314629a8aa674
khalid <rabaaouikhalid@gmail.com>
2025-11-14 20:40:22 +0100

    editt refreshtoken

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const generateTokens = (userId, role) => {
   const accessToken = jwt.sign({ userId, role }, process.env.JWT_SECRET, {
-    expiresIn: "1m",
+    expiresIn: "15m",
   });
 
   const refreshToken = jwt.sign(
